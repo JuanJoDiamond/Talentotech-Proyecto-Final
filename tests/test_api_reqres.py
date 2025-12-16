96bd50579cd6403d3575fa5c8940e4cf5dcf25c3
JuanJoDiamond <127349819+JuanJoDiamond@users.noreply.github.com>
2025-12-15 23:44:51 -0300

    Add files via upload
    "Revision Final"

--- tests/test_api_reqres.py
+++ tests/test_api_reqres.py
@@ -3,7 +3,6 @@ import pytest
 from utils.logger import logger
 
 # Obtener usuario
-@pytest.mark.skipif(reason="pide api key")
 def test_get_user(url_base,header_request):
     logger.info(f"Relizando la solitud GET a {url_base} ")
     response = requests.get(f"{url_base}/2",headers=header_request)
@@ -15,9 +14,7 @@ def test_get_user(url_base,header_request):
 
     logger.info("Validando el id dentro del usuario")
     assert data["data"]["id"] == 2
-    
 # Crear usuario
-@pytest.mark.skipif(reason="pide api key")
 def test_create_user(url_base,header_request):
     payload={
         "name": "Jose",
@@ -32,7 +29,6 @@ def test_create_user(url_base,header_request):
     assert data["name"] == payload["name"]
 
 # Eliminar usuario
-@pytest.mark.skipif(reason="pide api key")
 def test_delete_user(url_base,header_request):
     response = requests.delete(f"{url_base}/2",headers=header_request)
 
